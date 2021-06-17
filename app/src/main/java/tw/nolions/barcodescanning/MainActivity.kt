96e698b233ad58aaad17922ea9cab4d45e2809b0
NolionsChen <nolionschen@jetfuel.com.tw>
2021-06-17 15:07:44 +0800

    init

---
+++ app/src/main/java/tw/nolions/barcodescanning/MainActivity.kt
@@ -0,0 +1,193 @@
+package tw.nolions.barcodescanning
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import tw.nolions.barcodescanning.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
+
+private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+private const val TAG: String = "BarcodeScanning"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewBinding()
+
+        if (checkPermission()) {
+            bindCamera()
+        } else {
+            requestPermission()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            bindCamera()
+        } else {
+            Toast.makeText(this, "Camera permission required", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    /**
+     * ViewBinding
+     *
+     */
+    private fun viewBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    /**
+     * 權限檢查
+     *
+     */
+    private fun checkPermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+    /**
+     * 權限請求
+     *
+     */
+    private fun requestPermission() {
+        // opening up dialog to ask for camera permission
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun bindCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val previewUseCase = Preview.Builder().build().also {
+                it.setSurfaceProvider(binding.cameraView.surfaceProvider)
+            }
+
+            try {
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA // 使用後置鏡頭
+                val options = buildBarcodeScannerOptions()
+
+                // create BarcodeScanning Client
+                val scanner = BarcodeScanning.getClient(options)
+
+                // initialize and setting ImageAnalysis
+                val imageAnalysis = ImageAnalysis.Builder().build()
+                setAnalysis(imageAnalysis, scanner)
+
+                // Bind the cameraProvider to a LifecycleOwner.
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    previewUseCase,
+                    imageAnalysis
+                )
+            } catch (illegalStateException: IllegalStateException) {
+                Log.e(TAG, "illegalStateException" + illegalStateException.message.orEmpty())
+            } catch (illegalArgumentException: IllegalArgumentException) {
+                Log.e(TAG, "illegalArgumentException" + illegalArgumentException.message.orEmpty())
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    /**
+     * 圖像分析
+     *
+     * @param barcodeScanner
+     * @param imageProxy
+     */
+    private fun processImageProxy(barcodeScanner: BarcodeScanner, imageProxy: ImageProxy) {
+        imageProxy.image?.let { image ->
+            val inputImage =
+                InputImage.fromMediaImage(
+                    image,
+                    imageProxy.imageInfo.rotationDegrees
+                )
+
+            barcodeScanner.process(inputImage)
+                .addOnSuccessListener { barcodeList ->
+                    val barcode = barcodeList.getOrNull(0)
+                    // `rawValue` is the decoded value of the barcode
+                    barcode?.rawValue?.let { value ->
+                        // update our textView to show the decoded value
+                        binding.bottomText.text = "Barcode Value:$value"
+                    }
+                }
+                .addOnFailureListener {
+                    // This failure will happen if the barcode scanning model
+                    // fails to download from Google Play Services
+                    Log.e(TAG, "process fail, ${it.message.orEmpty()}")
+                }.addOnCompleteListener {
+                    imageProxy.image?.close()
+                    imageProxy.close()
+                }
+        }
+    }
+
+    /**
+     * 設置條碼格式
+     *
+     * @return
+     */
+    private fun buildBarcodeScannerOptions(): BarcodeScannerOptions {
+        return BarcodeScannerOptions.Builder().setBarcodeFormats(
+            Barcode.FORMAT_CODE_128,
+            Barcode.FORMAT_CODE_39,
+            Barcode.FORMAT_CODE_93,
+            Barcode.FORMAT_EAN_8,
+            Barcode.FORMAT_EAN_13,
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_UPC_A,
+            Barcode.FORMAT_UPC_E,
+            Barcode.FORMAT_PDF417
+        ).build()
+    }
+
+    /**
+     *  設置ImageAnalysis
+     *
+     * @param analysis
+     * @param scanner
+     */
+    private fun setAnalysis(analysis: ImageAnalysis, scanner: BarcodeScanner) {
+        analysis.setAnalyzer(
+            Executors.newSingleThreadExecutor(),
+            { imageProxy ->
+                processImageProxy(scanner, imageProxy)
+            }
+        )
+    }
+}
